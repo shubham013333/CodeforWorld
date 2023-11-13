8874545382316b021a1b4c09bd760d570d67ea71
Shubham Shrivas <60268544+shubham013333@users.noreply.github.com>
2023-11-13 18:38:46 +0530

    Update Sum of all Subarrays.py

--- f1.Subarrays/Final Destination/Sum of all Subarrays.py
+++ f1.Subarrays/Final Destination/Sum of all Subarrays.py
@@ -11,3 +11,14 @@ def SubArraySum(arr, n):
 arr = [1, 2]
 n = len(arr)
 print("Sum of SubArray :", SubArraySum(arr, n))
+
+#-----------------------O(N)--------------------------
+def SubArraySum(arr, n):
+	result = 0
+	for i in range(0, n):
+		result += (arr[i] * (i+1) * (n-i))
+	return result
+
+arr = [1, 2, 3]
+n = len(arr)
+print("Sum of SubArray : ",SubArraySum(arr, n))
